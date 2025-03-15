8f6835c43e6dd1b34d947c0208a8d9aa60f5c9d6
Zi Heng <pehz0007@e.ntu.edu.sg>
2025-03-16 01:14:16 +0800

    Added order tracker increment

---
+++ src/main/kotlin/com/ntu/sc2006/hawkerlah/service/Result.kt
@@ -0,0 +1,40 @@
+package com.ntu.sc2006.hawkerlah.service
+
+import com.ntu.sc2006.hawkerlah.service.toResponseEntity
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
+import org.springframework.http.ResponseEntity
+
+@Serializable
+enum class ResultStatus {
+    @SerialName("ok") Ok,
+    @SerialName("error") Error
+}
+
+@Serializable
+sealed class Result<T>(
+    val status: ResultStatus
+){
+    abstract val response: T?
+}
+@Serializable
+data class SuccessResult<T>(
+    override val response: T
+): Result<T>(ResultStatus.Ok)
+
+@Serializable
+data class ErrorResult<T>(
+    val message: String,
+    @Transient override val response: T? = null
+): Result<T>(ResultStatus.Error)
+
+inline fun <reified T> T.toResponseEntity(): ResponseEntity<String> {
+    val json = Json.encodeToString(this)
+    return ResponseEntity.ok(json)
+}
